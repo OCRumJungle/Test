6af2d9ca63660c081a81065827aae791730304dc
Monte Variakojis <monte@cobaltdigital.com>
2017-11-23 08:51:23 -0800

    Want to see if Doxy generated stuff will work on GH

---
+++ Software/Source/DoxygenMainPage.h
@@ -0,0 +1,26 @@
+#pragma once
+/*
+ * DoxygenMainPage.h
+ *
+ *  Created on: November 2017
+ *      Author: Monte Variakojis
+ *      This is the main documentation file for the project
+ */
+
+/**
+
+\mainpage GH Test Project
+
+# Some Title
+Stuff here
+
+## Subtitle
+More stuff here
+
+### Subsubtitle
+Even more stuff...
+
+*/
+
+
+
